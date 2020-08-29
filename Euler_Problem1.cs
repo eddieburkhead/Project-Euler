870dc9cb95e0238717e3068d61d9a6ec008d2f67
eddieburkhead <eddieburkhead@gmail.com>
2020-08-29 16:19:10 -0400

    Add files via upload

---
+++ Euler_Problem1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProjectEuler
+{
+    public static class Euler_Problem1
+    {
+        public static void Problem1()
+            {
+              int sum = 0;
+              for(int i = 0; i < 1000; i++) 
+               { 
+                  if (i % 3 == 0 || i % 5 == 0)
+                    {
+                      sum = sum + i;  
+                    } 
+                  else
+                    { 
+                      continue;
+                    }
+                }
+            }
+    }
+}
